3d6229dcec6f5995dbcf51fade49ad04940021a8
EduardoVilasBoasRSimoes <eduardovilasboasrsimoes@gmail.com>
2021-12-09 17:46:26 -0300

    Código do treino do ChatBot

---
+++ Chatbot training/TrainingChatbot.py
@@ -0,0 +1,23 @@
+import random
+
+import json
+
+import pickle
+
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
+
+lematizador = WordNetLemmatizer #Recebe a função para reduzir a palavra a sua forma canonica
+
+intents = json.loads(open('intents.JSON').read()) #Carregar o dicionario para a variavel intents
+
+palavras = []
+classes = []
+documentos = []
+ignore_letters = ['?', '!', '.', ','] #Caracteres que não serão levados em conta pela "leitura" das entradas
