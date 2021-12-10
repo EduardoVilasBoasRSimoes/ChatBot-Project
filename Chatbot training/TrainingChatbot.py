c9be4b4e9e13a724481617fde62ae6ae66c9126f
EduardoVilasBoasRSimoes <eduardovilasboasrsimoes@gmail.com>
2021-12-10 17:45:24 -0300

    Atualização do Treino

--- Chatbot training/TrainingChatbot.py
+++ Chatbot training/TrainingChatbot.py
@@ -7,25 +7,26 @@ import pickle
 import numpy as np
 
 import nltk
-from nltk import word_tokenize
-from nltk.stem import WordNetLemmatizer #Essa função serve para reduzir a palavra a sua forma canonica.
+from nltk.stem import WordNetLemmatizer  # Essa função serve para reduzir a palavra a sua forma canonica.
 
-#from tensorflow.keras.models import Sequential
-#from tensorflow.keras.layers import Dense, Activation, Dropout
-#from tensorflow.keras.optimizers import SGD
+# nltk.download('punkt', quiet=True)
 
-lematizador = WordNetLemmatizer #Recebe a função para reduzir a palavra a sua forma canonica
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense, Activation, Dropout
+# from tensorflow.keras.optimizers import SGD
 
-intents = json.loads(open('intents.JSON').read()) #Carregar o dicionario para a variavel intents
+lematizador = WordNetLemmatizer  # Recebe a função para reduzir a palavra a sua forma canonica
+
+intents = json.loads(open('intents.json').read())  # Carregar o dicionario para a variavel intents
 
 palavras = []
 classes = []
 documentos = []
-ignore_letters = ['?', '!', '.', ','] #Caracteres que não serão levados em conta pela "leitura" das entradas
+ignore_letters = ['?', '!', '.', ',']  # Caracteres que não serão levados em conta pela "leitura" das entradas
 
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-        listaPalavras = word_tokenize(pattern)
+        listaPalavras = nltk.word_tokenize(pattern)
         palavras.append(listaPalavras)
         documentos.append((listaPalavras, intent['tag']))
         if intent['tag'] not in classes:
